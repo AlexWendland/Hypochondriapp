f270432810dbb12080b30315488dfe3df47ee808
Jack Kirton <jackkirton@hotmail.co.uk>
2013-03-24 15:16:10 +0000

    Create MainManager and TwitterManager class.

---
+++ Server/src/com/group19/hypochondriapp/MainManager.java
@@ -0,0 +1,12 @@
+package com.group19.hypochondriapp;
+
+public class MainManager 
+{
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Server/src/com/group19/hypochondriapp/TwitterManager.java
@@ -0,0 +1,13 @@
+package com.group19.hypochondriapp;
+
+public class TwitterManager implements Runnable
+{
+
+	@Override
+	public void run() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
